7be636f1782be537ad5a4a251993da813d2e269b
Samandar Ochilov <ochilovsamandar@outlook.com>
2023-08-16 11:29:38 +0500

    AddDbContext and AddScoped methods add

--- OLXWebApi/Program.cs
+++ OLXWebApi/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using OLXWebApi.Data.DbContexts;
+using OLXWebApi.Data.IRepositories;
+using OLXWebApi.Data.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<OlxDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 
 var app = builder.Build();
 
