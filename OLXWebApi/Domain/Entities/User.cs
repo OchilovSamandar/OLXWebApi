c5f83a0eaa76dc1d55c2e775d58313d8494c91d4
Samandar Ochilov <ochilovsamandar@outlook.com>
2023-08-16 10:13:49 +0500

    User entity added

--- OLXWebApi/Domain/Entities/User.cs
+++ OLXWebApi/Domain/Entities/User.cs
@@ -1,7 +1,18 @@
-﻿namespace OLXWebApi.Domain.Entities
-{
-    public class User
-    {
+﻿using OLXWebApi.Domain.Entities.Commans;
+using OLXWebApi.Domain.Enums;
 
+namespace OLXWebApi.Domain.Entities
+{
+    public class User : Auditable
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public UserRole UserRole { get; set; }
+        public byte Awatar { get; set; }
+        public List<MyAds> MyAdsList { get; set; } = new List<MyAds>();
+        public List<Announcement> MyAnnouncementList { get; set; } = new List<Announcement>();
+        public bool IsActive { get; set; } = true;
     }
 }
