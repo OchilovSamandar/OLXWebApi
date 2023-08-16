b290b1480d5f0a81e398a53e7321fa6a7c3fa5bd
Samandar Ochilov <ochilovsamandar@outlook.com>
2023-08-16 10:07:03 +0500

    dbcontext class implement

--- OLXWebApi/Data/DbContexts/OlxDbContext.cs
+++ OLXWebApi/Data/DbContexts/OlxDbContext.cs
@@ -1,6 +1,15 @@
-﻿namespace OLXWebApi.Data.DbContexts
+﻿using Microsoft.EntityFrameworkCore;
+using OLXWebApi.Domain.Entities;
+
+namespace OLXWebApi.Data.DbContexts
 {
-    public class OlxDbContext
+    public class OlxDbContext: DbContext
     {
+        public OlxDbContext(DbContextOptions<OlxDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Announcement> Announcements { get; set; }
+        public DbSet<MyAds> MyAds { get; set; }
+        public DbSet<Category> Category { get; set; }
     }
 }
