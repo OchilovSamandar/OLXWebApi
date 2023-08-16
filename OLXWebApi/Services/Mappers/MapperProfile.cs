b11e1f58eac7fd16f0eb862caf88479c943e7a4f
Samandar Ochilov <ochilovsamandar@outlook.com>
2023-08-16 16:43:07 +0500

    Mapper

---
+++ OLXWebApi/Services/Mappers/MapperProfile.cs
@@ -0,0 +1,12 @@
+﻿
+using AutoMapper;
+using OLXWebApi.Domain.Entities;
+using OLXWebApi.Services.Dtos;
+
+namespace OLXWebApi.Services.Mappers
+{
+    public class MapperProfile : Profile 
+    {
+        //CreateMap<User, UserForCreationDto>().ReverseMap();
+    }
+}
