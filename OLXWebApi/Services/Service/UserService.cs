ea6fa9d22d76e97a6918bbf0571c55bf25965cf6
Samandar Ochilov <ochilovsamandar@outlook.com>
2023-08-17 11:30:34 +0500

    ModifyAsync method add

--- OLXWebApi/Services/Service/UserService.cs
+++ OLXWebApi/Services/Service/UserService.cs
@@ -1,10 +1,12 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.EntityFrameworkCore;
 using OLXWebApi.Data.IRepositories;
 using OLXWebApi.Domain.Entities;
 using OLXWebApi.Services.Dtos;
 using OLXWebApi.Services.Exceptions;
 using OLXWebApi.Services.IService;
+using OLXWebApi.Shared.Helper;
 
 namespace OLXWebApi.Services.Service
 {
@@ -24,9 +26,26 @@ namespace OLXWebApi.Services.Service
             throw new NotImplementedException();
         }
 
-        public ValueTask<User> ModifyAsync(long id, UserForCreationDto dto)
+        public async ValueTask<UserForResultDto> ModifyAsync(long id, UserForCreationDto dto)
         {
-            throw new NotImplementedException();
+            var oldUser = await this._repository.SelectByIdAsync(id);
+            if (oldUser == null)
+                throw new OlxException(404, "User not found");
+
+            var exsistUser = await this._repository.SelectAll()
+                .FirstOrDefaultAsync(e => e.Email == dto.Email);
+            if (exsistUser != null && oldUser.Email != dto.Email)
+                throw new OlxException(400, "Email is already taken");
+
+            var user = this._mapper.Map(dto, exsistUser ?? oldUser);
+
+            user.Id = id;
+            user.Password =PasswordHelper.Hash(dto.Password);
+            user.UpdatedAt = DateTime.UtcNow;
+            var result = await this._repository.UpdateAsync(user);
+            await this._repository.SaveChangesAsync();
+
+            return this._mapper.Map<UserForResultDto>(result);
         }
 
         public async ValueTask<bool> RemoveAsync(long id)
