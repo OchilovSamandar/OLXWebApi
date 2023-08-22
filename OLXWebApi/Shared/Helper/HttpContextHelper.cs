37ec9fc33be09c6e5082694d459d0297e82792da
Samandar Ochilov <ochilovsamandar@outlook.com>
2023-08-22 14:56:16 +0500

    User role bug fix

---
+++ OLXWebApi/Shared/Helper/HttpContextHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace OLXWebApi.Shared.Helper
+{
+    public class HttpContextHelper
+    {
+        public static IHttpContextAccessor Accessor { get; set; }
+        public static HttpContext HttpContext => Accessor?.HttpContext;
+        public static string UserRole => HttpContext?.User?.FindFirst("role")?.Value;
+        public static long? UserId => long.TryParse(HttpContext?.User?.FindFirst("id")?.Value, out _tempUserId) ? _tempUserId : null;
+
+        private static long _tempUserId;
+    }
+}
