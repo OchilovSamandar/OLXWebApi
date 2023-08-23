44ae3a03d1246d652e850d40f9fe21d482d9ac0a
Samandar Ochilov <ochilovsamandar@outlook.com>
2023-08-23 19:29:55 +0500

    Authorization not work

---
+++ OLXWebApi/Controllers/WebApiController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OLXWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "Admin")]
+    public class WebApiController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello OLX";
+        }
+    }
+}
