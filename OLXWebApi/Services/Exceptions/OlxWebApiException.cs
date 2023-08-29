54455afbb2bd413895787fcf0eb77a9df0d9242b
Samandar Ochilov <ochilovsamandar@outlook.com>
2023-08-29 17:53:46 +0500

    GetUserById method edit

---
+++ OLXWebApi/Services/Exceptions/OlxWebApiException.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.Serialization;
+
+namespace OLXWebApi.Services.Exceptions
+{
+    [Serializable]
+    internal class OlxWebApiException : Exception
+    {
+        public int Status { get; set; }
+        public OlxWebApiException(int status = 500, string message = "Something went wrong") :base(message) {
+        
+           this.Status = status;
+        
+        }
+
+    }
+}
