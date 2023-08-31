e7972fdbf36d55b635a7a1836d9ccdff629a24eb
Samandar Ochilov <ochilovsamandar@outlook.com>
2023-08-31 16:14:32 +0500

    Role service adding

---
+++ OLXWebApi/Services/Dtos/Role/RoleForUpdateDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OLXWebApi.Services.Dtos.Role
+{
+    public class RoleForUpdateDto
+    {
+        [Required]
+        public long RoleId { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
---
+++ OLXWebApi/Services/Service/RoleService.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using OLXWebApi.Data.IRepositories;
+using OLXWebApi.Domain.Entities;
+using OLXWebApi.Services.Dtos.Role;
+using OLXWebApi.Services.IService;
+
+namespace OLXWebApi.Services.Service
+{
+    public class RoleService : IRoleService
+    {
+        private readonly IMapper _mapper;
+        private readonly IRepository<Role> _repository;
+
+        public ValueTask<bool> AssignRoleForUserAsync(long userId, long roleId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<bool> CheckRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async ValueTask<RoleForResultDto> CreateRoleAsync(RoleForCreationDto dto)
+        {
+            await _repository.SelectAll().FirstOrDefaultAsync(r => r.Name.Equals(dto.Name));
+            return;
+        }
+
+        public ValueTask<bool> DeleteRoleAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<IEnumerable<RoleForResultDto>> RetriveAllRoleAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<Role> RetriveRoleByIdAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<RoleForResultDto> UpdateRoleAsync(RoleForUpdateDto dto, long id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
