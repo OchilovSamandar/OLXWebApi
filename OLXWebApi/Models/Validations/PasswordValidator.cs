1d4e12679693698e21dd857de9d26cfa7fdaa631
Samandar Ochilov <ochilovsamandar@outlook.com>
2023-09-06 12:01:33 +0500

    Attributes add

---
+++ OLXWebApi/Models/Validations/PasswordValidator.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace OLXWebApi.Models.Validations
+{
+    public static class PasswordValidator
+    {
+        public static (bool isValid , string Message) IsStrong(string passowrd)
+        {
+            bool isDigit = passowrd.Any(x => char.IsDigit(x));
+            if (!isDigit)
+                return (false, "Password must contain at least 1 digit nummber");
+            bool isUppercase = passowrd.Any(x=> char.IsUpper(x));
+            if (!isUppercase)
+                return (false, "Password must contain at least 1 uppercase character");
+
+            return (true, "Valid password");
+        }
+    }
+}
